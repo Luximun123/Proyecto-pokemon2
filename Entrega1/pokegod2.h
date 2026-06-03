a48b3a9fc4b5d1fb713a17d60eed50ae458d3ef0
Beny Urbina <urbinagilbenyjose@gmail.com>
2026-06-02 22:55:49 -0400

    variables dadas por el profe (proximamente)

--- Entrega1/pokegod2.h
+++ Entrega1/pokegod2.h
@@ -1,4 +1,26 @@
 #ifndef POKEGOD2_H
 #define POKEGOD2_H
+#include <string>
+using namespace std;
+
+class Pokemones { //la clase para poner a las cabras que voy a crear (su cedula)
+
+    private: // valores del profesor
+    string elnombre;
+    /*  string
+    string
+    int vida
+    int salud;
+
+        */
+};
+
+
+
+
+
+
+
+
 
 #endif
